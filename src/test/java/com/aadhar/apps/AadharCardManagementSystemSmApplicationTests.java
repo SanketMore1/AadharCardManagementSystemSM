01955336575366f61d84a5e79f4ce642a29d1c29
SanketMore1 <70767308+SanketMore1@users.noreply.github.com>
2021-01-01 18:40:32 +0530

    First Commit

---
+++ src/test/java/com/aadhar/apps/AadharCardManagementSystemSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadhar.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadharCardManagementSystemSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
